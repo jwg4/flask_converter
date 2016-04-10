34b34a741d42b2232b81ce927dbc0348e9a6bc77
Jack <jack.grahl@yahoo.co.uk>
2016-04-11 00:10:04 +0100

    Try to register a converter for backwards arguments.

--- flask_converter/__init__.py
+++ flask_converter/__init__.py
@@ -1,3 +1,16 @@
+from werkzeug.routing import BaseConverter
+
+class BackwardsConverter(BaseConverter):
+    def to_python(self, value):
+        return value[::-1]
+    def to_url(self, value):
+        return value[::-1]
+
 class Converter(object):
-    pass
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
     
+    def init_app(self, app):
+        app.url_map.converters['backwards'] = BackwardsConverter
