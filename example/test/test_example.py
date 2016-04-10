d486f00910a43a910bc66e76efff9871d7028bd9
Jack <jack.grahl@yahoo.co.uk>
2016-04-11 00:13:32 +0100

    Typos in unit test code.

--- example/test/test_example.py
+++ example/test/test_example.py
@@ -5,11 +5,11 @@ class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
         my_app.app.config['TESTING'] = True
-        self.client = flaskr.app.test_client()
+        self.client = my_app.app.test_client()
 
     def test_hi(self):
         response = self.client.get('/hi/')
-        assertIsEqual(response.data, 'Hi there')
+        self.assertEqual(response.data, 'Hi there')
 
 if __name__ == '__main__':
     unittest.main()
